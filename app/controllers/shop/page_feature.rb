5c55d88703fa6c9d7c51cec5479a9fda169f8e1c
Pascal Rettig <pascal@cykod.com>
2009-04-13 08:43:43 -0400

    fix

--- app/controllers/shop/page_feature.rb
+++ app/controllers/shop/page_feature.rb
@@ -209,7 +209,7 @@ class Shop::PageFeature < ParagraphFeature
   FEATURE
 
   def shop_product_detail_feature(data)
-    webiva_feature('shop_product_detail',data) do |c|
+    webiva_feature('shop_product_detail') do |c|
       c.define_tag 'no_product' do |tag|
         data[:product] ? nil : tag.expand
       end
