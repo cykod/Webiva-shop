94b0a3fdd7623b5722afd596799568cd7911ac0e
Pascal Rettig <pascal@cykod.com>
2011-10-20 16:13:13 -0400

    Address fixes

--- app/models/shop/shop_region.rb
+++ app/models/shop/shop_region.rb
@@ -6,7 +6,7 @@ class Shop::ShopRegion < DomainModel
   validate :region_countries_logic
   
   has_many :countries, :class_name => 'Shop::ShopRegionCountry', :dependent => :delete_all, :order => 'country'
-  has_many :subregions, :class_name => "Shop::ShopSubregion", :dependent => :delete_all
+  has_many :subregions, :class_name => "Shop::ShopSubregion", :dependent => :delete_all, :order => 'shop_subregions.name'
   has_options :subregion_type, [ [ 'State', 'state' ], [ 'Province','province' ], ['Region','region' ], ['District','district'], ['Area','area'] ]
 
   has_many :shipping_categories, :class_name => "Shop::ShopShippingCategory", :dependent => :destroy
